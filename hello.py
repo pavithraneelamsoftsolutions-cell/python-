d5ba9716aafaa529f791066dbef58cc6c5eb212c
pavithraneelamsoftsolutions-cell <pavithra.neelamsoftsolutions@gmail.com>
2026-06-06 13:28:11 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,115 @@
+# student_name = "kaviya"
+# print(student_name)
+
+# age = 20
+# print(age)
+
+# salary = 25000.00
+# print(salary)
+
+# is_student = True
+# print(is_student)
+
+
+# name ='raja'
+# age =23
+# height=5.8
+# is_student = True
+
+# print(type(name))
+# print(type(age))
+# print(type(height))
+# print(type(is_student))
+
+# name = input("enter your name:")
+# print(name )
+ 
+
+# name = input("enter your name:")
+# print(name)
+# college = input("enter your college:")
+# print(college )
+# age= input("enter your age:")
+# print(age)
+# salary =input('eneter your salary:')
+# print(salary)
+# is_student=input('enter true or flase')
+# print(is_student)
+
+
+# age ="20"
+# print(type(age))
+
+
+# num ="100"
+# num = int(num)
+# print(num)
+# print(type(num))
+
+ 
+# age= 24
+# age= float(age)
+# print(age)
+# print(type(age))
+
+
+# mark =459
+# mark = str(mark)
+# print(mark)
+# print(type(mark)) 
+
+
+# # small add two number 
+# a= input("enter first number:")
+# b=input("enter second number:")
+# print(a+b)
+
+
+# a= int(input("enter first number:"))
+# b=int(input("enter second number:"))
+# print(a+b)
+
+
+# a=10
+# b=4
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+# arithmetic  program :
+# a=10
+# b=3
+
+# print("addition=",a+b)
+# print("subtraction=",a-b)
+# print("multiplication=",a*b)
+# print("division=",a/b)
+# print('modulus=',a%b)
+# print('floor division=',a//b)
+# print("power=",a**b)
+
+# print (10==10)
+
+# print(20!=20)
+
+# print(5>20)
+
+# # <less than
+# print(10<5)
+
+# # >= greaterthan or equal
+
+
+# age =19
+# print(age>=18)
+
+# # <= less than or equal
+# age=18
+# print(age<=18)
+
+tamil =80
+english =70 
+total = tamil+english 
+print("total marks=",total)
+print(total>=150)
