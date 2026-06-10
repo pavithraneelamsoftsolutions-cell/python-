c1e024effe924d308371ce99bd096198c1f8ed8c
pavithraneelamsoftsolutions-cell <pavithra.neelamsoftsolutions@gmail.com>
2026-06-10 19:51:45 +0530

    Add files via upload

---
+++ assignment_operators.py
@@ -0,0 +1,19 @@
+a=20
+print("initial value=",a)
+a+=5
+print("after +=",a)
+a-=3
+print("after -=",a)
+a*=2
+print("after *=",a)
+a/=4
+print("after /=",a)
+a%=3
+print("after %=",a)
+
+a//=1  
+print("after //=",a)
+a**=2  //a==2
+print("after **=",a)
+
+
