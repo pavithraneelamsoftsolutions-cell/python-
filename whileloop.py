dab57aba5ed929170e531925fc1f216e65301ab9
pavithraneelamsoftsolutions-cell <pavithra.neelamsoftsolutions@gmail.com>
2026-06-17 10:14:48 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,44 @@
+# i = 1
+# while i<=5:
+#    print(i)
+#    i+=1
+
+# # print name 5 times
+
+# count =1
+# while count<=5:
+#     print("pavi")
+#     count +=1
+# # print even number :
+
+# num=1 
+# while num <=10:
+#     print(num)
+#     num +=2 
+
+# counter  timer  
+# count  =5
+# while  count >=1:
+#     print(count)
+#     count -=1
+# print("times up!")
+
+# infinite loop 
+# if  we  forgot  to update  the variable the loop never  stop 
+# count =1
+# while count<=5:
+#     print (count)
+
+
+# while  loop  with user  input 
+# name =""
+# while name =="":
+#     name =input("enter your name:")
+#     print("welcome",name)
+
+#  guess  the number  
+number  =5
+guess =0
+while guess !=number:
+    guess = int(input("enter  the  guess number (1-10):"))
+print("corret guess")
