c16e81c138e8f8c2512efed9a8e78bb561ff1700
pavithraneelamsoftsolutions-cell <pavithra.neelamsoftsolutions@gmail.com>
2026-06-17 19:56:18 +0530

    Add files via upload

---
+++ break.py
@@ -0,0 +1,40 @@
+# for  i in range (1,11):
+#     if i==5:
+#       break
+#     print(i)
+
+
+#  stop  at  roll_no 7
+
+# for roll_no in range(1,11):
+#     if roll_no==7:
+#         break
+#     print("roll_no",roll_no)
+
+
+# i=1
+# while i<=10:
+#     if i==5:
+#         break
+#     print(i)
+#     i+=1
+
+# for  i  in range (1,6):
+#   if i==3:
+#     continue
+#   print(i)
+
+
+# #  print  odd  number  
+# for  i  in range(1,11):
+#     if i%2!=0:
+#         continue
+#     print(i)
+
+
+i=1
+while i<20:
+    i+=1
+    if i%3==0:
+        continue
+    print(i)
