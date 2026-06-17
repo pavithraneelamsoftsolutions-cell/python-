f6207e1aef29a3bbc4c3b073f9852696248b9792
pavithraneelamsoftsolutions-cell <pavithra.neelamsoftsolutions@gmail.com>
2026-06-17 19:58:40 +0530

    Add files via upload

---
+++ whileloop1.py
@@ -0,0 +1,15 @@
+# multiplication table  used in while loop 
+
+# i =1
+# while i<=20:
+#     print("5x",i,"=",5*i)
+#     i+=1
+
+
+# sum of first 10 numbers 
+i=1
+total=0
+while i<=10:
+    total +=i
+    i+=1
+print("sum=",total)
