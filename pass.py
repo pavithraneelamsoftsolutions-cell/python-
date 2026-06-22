d148e2953c170c5c26b89e215b6e753907e774b8
pavithraneelamsoftsolutions-cell <pavithra.neelamsoftsolutions@gmail.com>
2026-06-22 19:49:30 +0530

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,58 @@
+# age =20
+# if age >=18:
+#     pass
+# print("program finished")
+
+
+# for i in range(5):
+#     pass
+# print("loop completed")
+
+
+
+# # continue
+# for i in range(5):
+#     if i ==2:
+#         continue
+#     print(i)
+
+# for i in range(5):
+#     if i ==2:
+#         pass
+#     print(i)
+
+# for i in range(5):
+#     if i ==2:
+#         break
+#     print(i)
+
+
+# for i in range(5):
+#     if i ==2:
+#         pass
+#     print(i)
+
+# username ="ravi"
+# if username=="admin":
+#    pass
+# else:
+#    print("invalied user")
+
+# print("program is running")
+
+# for i in range (1,4):
+#     for j in range(1,4):
+#         print(i,j)
+
+# printing star
+# for i in range(3):
+#     for j in range(3):
+#         print("*" ,end=" ")
+#     print()
+
+# number patten:
+for i in range(3):
+    for j in range(1,4):
+        print(j,end=" ")
+
+    print()
