c126513f900aa11118dd7119d94786e469da19f0
pavithraneelamsoftsolutions-cell <pavithra.neelamsoftsolutions@gmail.com>
2026-06-23 19:48:46 +0530

    Add files via upload

---
+++ nested_forloop.py
@@ -0,0 +1,39 @@
+# mulitiplication grid:
+# for i in range(1,4):
+#     for j in range(1,4):
+#         print(i*j,end=" ")
+#     print()
+
+# student attendance 
+
+# day 
+# and  student 
+
+# for day in range (1,4):
+#     for student in range(1,4):
+#         print("day",day,"student",student)
+#     print()
+
+# rectangular patten :
+# for  i in range(4):
+#     for j in range(4):
+#         print("*", end=" ")
+#     print()
+
+#  triangle star
+# for i in  range(1,6):
+#     for j in range(i):
+#         print("*" ,end=" ")
+#     print()
+
+# for i in range (5,0,-1):
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+#  number triangle:
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j ,end=" ")
+    print()
