2ae25244173222312a723241822db0fcaa8ef391
pavithraneelamsoftsolutions-cell <pavithra.neelamsoftsolutions@gmail.com>
2026-06-30 19:57:42 +0530

    Add files via upload

---
+++ stringmethod.py
@@ -0,0 +1,23 @@
+# name = "python"
+# print(name.upper())
+# print(name.lower())
+
+#  title 
+# name = "python programming  used  in everywhere"
+# print(name.title()) 
+
+# replace:
+# text = "i love java"
+# text1 =  text.replace("java","python")
+# print(text1.upper())
+# print(text1.upper())
+# print(text.title())
+# print(text1.title())
+# print(text.find("v"))
+
+
+# fruits="banana"
+# print(fruits.count("a"))
+# print(fruits.count("n"))
+# print(fruits.count("b"))
+# print(text.split())
