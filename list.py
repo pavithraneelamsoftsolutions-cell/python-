d97bb78c82f5660a2ee26756224f547789256869
pavithraneelamsoftsolutions-cell <pavithra.neelamsoftsolutions@gmail.com>
2026-07-03 19:51:00 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,47 @@
+fruits =["apple","grapes","mango","pineapple"]
+# print(fruits[0])
+# print(fruits[1])
+# print(fruits[2])
+# print(fruits[3])
+print(fruits[-1])
+print(fruits[-2])
+print(fruits[-3])
+print(fruits[-4])
+
+
+
+string_list = ["apple","mango","banana"]
+
+string_list[1]="watermelon"
+print(string_list)
+
+
+
+# length of  list :
+
+# string_list = ["apple","mango","banana","mango1"]
+# print(len(string_list))
+
+# list traversal:
+# student =["ravi","rahul","kalai"]
+# for  name  in student:
+#     print(name)
+
+# list  slicing:
+# student =["ravi","rahul","kalai","kumar","devi"]
+# print(student[0:4])
+
+# append:
+# student =["ravi","rahul","kalai","kumar","devi"]
+# student.append("priya")
+# print(student)
+
+
+# insert()
+# student =["ravi","rahul","kalai","kumar","devi"]
+# student.insert(1,"priya")
+# print(student)
+
+student =["ravi","rahul","kalai","kumar","devi"]
+student.remove("kalai")
+print(student)
