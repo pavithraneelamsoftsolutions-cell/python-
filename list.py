4b62a8271cc4e5766ec18c71b99f2d092def350e
pavithraneelamsoftsolutions-cell <pavithra.neelamsoftsolutions@gmail.com>
2026-07-04 10:39:50 +0530

    Add files via upload

--- list.py
+++ list.py
@@ -1,19 +1,19 @@
-fruits =["apple","grapes","mango","pineapple"]
-# print(fruits[0])
-# print(fruits[1])
-# print(fruits[2])
-# print(fruits[3])
-print(fruits[-1])
-print(fruits[-2])
-print(fruits[-3])
-print(fruits[-4])
+# fruits =["apple","grapes","mango","pineapple"]
+# # print(fruits[0])
+# # print(fruits[1])
+# # print(fruits[2])
+# # print(fruits[3])
+# print(fruits[-1])
+# print(fruits[-2])
+# print(fruits[-3])
+# print(fruits[-4])
 
 
 
-string_list = ["apple","mango","banana"]
+# string_list = ["apple","mango","banana"]
 
-string_list[1]="watermelon"
-print(string_list)
+# string_list[1]="watermelon"
+# print(string_list)
 
 
 
@@ -42,6 +42,97 @@ print(string_list)
 # student.insert(1,"priya")
 # print(student)
 
-student =["ravi","rahul","kalai","kumar","devi"]
-student.remove("kalai")
-print(student)
+
+# remove
+# student =["ravi","rahul","kalai","kumar","devi"]
+# student.remove("kalai")
+# print(student)
+
+# pop()
+# student =["ravi","rahul","kalai","kumar","devi"]
+# student.pop()
+# print(student)
+
+# # sort() assending order
+# student =["ravi","rahul","kalai","kumar","devi"]
+# student.sort()
+# print(student)
+# # sort() descending order
+# student =["ravi","rahul","kalai","kumar","devi"]
+# student.sort(reverse=True)
+# print(student)
+
+# # reverse  ()
+# student =["ravi","rahul","kalai","kumar","devi"]
+# student.reverse()
+# print(student)
+
+# # clear()
+# student =["ravi","rahul","kalai","kumar","devi"]
+# student.clear()
+# print(student)
+
+# # copy()
+# student =["ravi","rahul","kalai","kumar","devi"]
+# new_student=student.copy()
+# print(new_student)
+
+# # extend
+# student =["ravi","rahul","kalai","kumar","devi"]
+# new_student= ["zara","kavitha"]
+# student.extend(new_student)
+# print(student)
+
+# # index()
+# student =["ravi","rahul","kalai","kumar","devi"]
+# print(student.index("ravi"))
+# print(student.index("rahul"))
+# print(student.index("kalai"))
+# print(student.index("kumar"))
+# print(student.index("devi"))
+
+
+number=[10,20,10,40,60,10]
+print(number.count(10))
+
+student =["ravi","rahul","kalai","kumar","devi","ravi","devi","ravi"]
+print(student.count("ravi"))
+print(student.count("devi"))
+
+
+# create empty list 
+
+list=[]
+
+# add 5 number using append
+
+list.append(10)
+list.append(20)
+list.append(30)
+list.insert(2,60)
+print(list)
+
+list.remove(30)
+print(list)
+print(len(list))
+print(list.count(10))
+# find the index of python 
+lang=["java","python","c"]
+print(lang.index("python"))
+
+
+students =["ravi","rahul","kalai","kumar","devi","ravi","devi","ravi"]
+unique_student=[]
+for student in students: 
+    if student not in unique_student:
+        unique_student.append(student)
+print("original ",students)
+print("unique student",unique_student)
+
+
+# student mark manager:
+marks=[]
+for i in range(5):
+    mark= int(input(f"enter mark {i+1}:"))
+    marks.append(mark)
+print("\n marks:",marks)
