79049c7b01145e319c7e8f02fabb6c318bfcd03b
pavithraneelamsoftsolutions-cell <pavithra.neelamsoftsolutions@gmail.com>
2026-07-08 19:58:08 +0530

    Add files via upload

--- identity.py
+++ identity.py
@@ -1,40 +1,64 @@
-a=[10,20]
-b=[10,20]
-print("is opertr", a is b)
-
-a=[10,20]
-b=a
-print("is opertor :",a is b)
-
-# # == vs is 
 # a=[10,20]
 # b=[10,20]
+# print("is opertr", a is b)
 
-# print(a==b)
+# a=[10,20]
+# b=a
+# print("is opertor :",a is b)
+
+# # # == vs is 
+# # a=[10,20]
+# # b=[10,20]
+
+# # print(a==b)
 
 
-# is not :
-a=[1,2]
-b=[1,2]
-print ("is not ",a is not b)
+# # is not :
+# a=[1,2]
+# b=[1,2]
+# print ("is not ",a is not b)
 
-a=[1,2]
+# a=[1,2]
+# b=a
+# print("is not",a is not b)
+
+# name = None
+# print( name is None)
+
+# a= None
+# if a is None:
+#     print("variable is empty")
+# else:
+#     print("variable has value")
+
+
+# lang=["python","java","c"]
+# search_lag="ruby"
+# if search_lag not in lang:
+#     print(f"yes,{search_lag} is not in the list")
+# else:
+#     print(f"no,{search_lag} is already in the list")
+
+
+a=[10,20]
+b=[10,20]
+print("is operator:",a is b)
+
+a=[10,20]   
 b=a
-print("is not",a is not b)
+print("is operator:",a is b)
+
+a=[10,20]
+b=[10,20]
+print("is not operator:",a is not b)
+
+a=[10,20]   
+b=a
+print("is not operator:",a is not  b)
+
 
 name = None
-print( name is None)
-
-a= None
-if a is None:
+if  name  is None:
     print("variable is empty")
 else:
-    print("variable has value")
-
-
-lang=["python","java","c"]
-search_lag="ruby"
-if search_lag not in lang:
-    print(f"yes,{search_lag} is not in the list")
-else:
-    print(f"no,{search_lag} is already in the list")
+    print("variables have value")
