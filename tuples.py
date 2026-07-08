178fa3726b78daadc0609b4d010539c209a0487b
pavithraneelamsoftsolutions-cell <pavithra.neelamsoftsolutions@gmail.com>
2026-07-08 19:57:33 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,42 @@
+data=(10,20,30,40,50)
+print(data)
+
+# sting tuples 
+data =("apple","mango","orange")
+# mixed data types:
+data=("apple",10,2.5,True)
+
+# data =("apple","mango","orange")
+# data=("apple",10,2.5,True)
+# print(data[0])
+# print(data[1])
+# print(data[2])
+# print(data[3])
+
+# print(data[-1])
+# print(data[-2])
+# print(data[-3])
+# print(data[-4])
+
+data =("apple","mango","orange")
+print(len(data))
+
+# loop in tuples:
+datas =("apple","mango","orange")
+for data in  datas:
+    print(data)
+
+# membership operator
+datas =("apple","mango","orange")
+print("mango" in  datas)
+
+# count()
+number =(10,20,30,30,30,20,10,10)
+print(number.count(10))
+print(number.count(30))
+
+# slicing:
+
+datas =("apple","mango","orange")
+print(datas[0:1])
+print(datas[2:3])
